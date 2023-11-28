92662c7db20a48e151ef4b4e8abb0edef0c855ab
chinnapareddy <chinna.aduri@gmail.com>
2023-11-28 05:58:24 -0800

    Roboshop

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "p1-state-tf"
+key    = "dev/terraform.tfstate"
+region = "us-east-1"
---
+++ output.tf
@@ -0,0 +1,15 @@
+/*output "vpc" {
+  value = module.vpc
+}*/
+
+/*
+output "alb" {
+ value = lookup(lookup(lookup(module.alb, "private", null),"alb", null),"dns_name", null)
+}*/
+
+
+/*
+output  "rds" {
+ value = module.rds
+}
+*/
